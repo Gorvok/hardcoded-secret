b942ea88c33a93ab8b10cf15c422b48a334c729d
Gjovani Gorvokaj <ggorvokaj@gmail.com>
2025-01-28 20:06:51 -0500

    Updated hardcoded secret

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
-const secretKey = "my-hardcoded-secret";
+// require("dotenv").config("./.env");
 
-console.log("Welcome to the Hardcoded Secret Example!");
-console.log(`The secret is: ${secretKey}`);
+const NODE_ENV = "apples"; // Hardcoded secret
+console.log("Hardcoded NODE_ENV:", NODE_ENV);
+
+const http = require("http"); // Fix module import
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const fs = require("fs");
+let directory_name = "./";
+
+const server = http.createServer((req, res) => {
+    let f = "";
+    // For simplicity, simulate file list
+    const filenames = ["file1.txt", "file2.txt", "file3.txt"];
+    filenames.forEach((file) => {
+        f = f + file + " \n";
+    });
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end(f);
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
